ce3c52370379983fe3f7a24657d01c7a2cf86fc5
Roy Pfaffman <pair+roy.pfaffman@socialchorus.com>
2014-08-19 12:37:37 -0700

    Creates channel for user input that comes from a fanOut readInput function.

--- async-commander.go
+++ async-commander.go
@@ -8,53 +8,48 @@ import (
 	"strings"
 )
 
-// func main() {
-// 	var input string
-// 	for {
-// 		fmt.Scanf("%s", &input)
-// 		input = strings.Join([]input, " ")
-// 		fmt.Println("EXE:", input)
-// 		//input = strings.Join(input, " ")
-// 		//exe_cmd(input)
-// 	}
-// }
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	var input string
+	c := read_input()
+
 	fmt.Print(prompt())
 
-	for scanner.Scan() {
-		input = scanner.Text()
-		fmt.Println(input)
-		exe_cmd(input)
+	for {
+		select {
+		case output := <-c:
+			fmt.Println(output)
+		}
 		fmt.Print(prompt())
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
 
-func exe_cmd(cmd string) {
-	// splitting head => g++ parts => rest of the command
+func read_input() <-chan string {
+	c := make(chan string)
+	scanner := bufio.NewScanner(os.Stdin)
+	var input string
+	go func() {
+		for scanner.Scan() {
+			input = scanner.Text()
+			c <- exe_cmd(input)
+		}
+	}()
+	return c
+}
+
+func exe_cmd(cmd string) string {
 	parts := strings.Fields(cmd)
 	head := parts[0]
 	parts = parts[1:len(parts)]
 
 	if head == "cd" {
-		fmt.Println("You tried to change dirs", parts[0])
 		os.Chdir(parts[0])
-		return
+		return fmt.Sprintln()
 	} else {
 		out, err := exec.Command(head, parts...).Output()
 		if err != nil {
-			fmt.Printf("%s", err)
+			return fmt.Sprintf("%s", err)
 		}
-		fmt.Printf("%s", out)
+		return fmt.Sprintf("%s", out)
 	}
-
-	// wg.Done() // Need to signal to waitgroup that this goroutine is done
 }
 
 func prompt() string {
