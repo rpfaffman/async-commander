15da5d38eb93b3aa8b03cdc27dfd11674566092c
Roy Pfaffman <pair+roy.pfaffman@socialchorus.com>
2014-08-19 13:42:13 -0700

    Commands are async!

--- async-commander.go
+++ async-commander.go
@@ -24,32 +24,50 @@ func main() {
 
 func read_input() <-chan string {
 	c := make(chan string)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	var input string
 	go func() {
 		for scanner.Scan() {
 			input = scanner.Text()
-			c <- exe_cmd(input)
+			output_channel := exe_cmd(input)
+
+			// currently this channel is not really used and the process
+			// prints directly to stdout.  we need to find a way to pipe
+			// this into the channel.
+			go func() {
+				c <- <-output_channel
+			}()
 		}
 	}()
+
 	return c
 }
 
-func exe_cmd(cmd string) string {
+func exe_cmd(cmd string) <-chan string {
+	c := make(chan string)
+
 	parts := strings.Fields(cmd)
 	head := parts[0]
 	parts = parts[1:len(parts)]
 
-	if head == "cd" {
-		os.Chdir(parts[0])
-		return fmt.Sprintln()
-	} else {
-		out, err := exec.Command(head, parts...).Output()
-		if err != nil {
-			return fmt.Sprintf("%s", err)
+	go func() {
+		if head == "cd" {
+			os.Chdir(parts[0])
+			c <- fmt.Sprint()
+		} else {
+			// write now, output does not go into the channel;
+			// it prints directly to the stdout.  find a way to
+			// send it into the channel.
+			cmd := exec.Command(head, parts...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stdout
+			cmd.Run()
+			c <- fmt.Sprintln()
 		}
-		return fmt.Sprintf("%s", out)
-	}
+	}()
+
+	return c
 }
 
 func prompt() string {
