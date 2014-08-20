e14d8a7a72f529fbfc54a40273cf81f8764afa27
Roy Pfaffman <pair+roy.pfaffman@socialchorus.com>
2014-08-19 23:19:13 -0700

    Processes are now listed and labeled.  They're tagged with an incremental identifier, and there's a reference to their channel.  Still unable to send input to the channel, however.  Also some processes seem to mysteriously delete themselves from the list (rails s), even though they continue to run in the background.
    Consider using os.Process instead of the exec.Command.
    
    Further research shows that you might need to use pipes with os/exec for
    input and output to the command.
    
    http://golang.org/pkg/os/exec/

--- async-commander.go
+++ async-commander.go
@@ -1,3 +1,11 @@
+// OBJECTIVES
+
+// label processes
+// - send commands to those processes
+// - mute/unmute those processes
+
+// have outputs of process go through channel
+
 package main
 
 import (
@@ -5,13 +13,20 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
-func main() {
-	c := read_input()
+type Process struct {
+	identifier string
+	command    string
+	channel    chan string
+}
 
-	fmt.Print(prompt())
+var PROCESSES map[string]Process = make(map[string]Process)
+
+func main() {
+	c := fan_in()
 
 	for {
 		select {
@@ -22,21 +37,40 @@ func main() {
 	}
 }
 
-func read_input() <-chan string {
+func create_process(command string) Process {
+	identifier := strconv.Itoa(len(PROCESSES) + 1)
+	channel := exe_cmd(command, identifier)
+	process := Process{identifier, command, channel}
+	PROCESSES[identifier] = process
+	return process
+}
+
+// This function should create processes and keep
+// track of them.  Maybe build a map of identifier to
+// process struct.
+//
+// It also maintains its own output channel through
+// which all output is fanned in.
+func fan_in() <-chan string {
 	c := make(chan string)
+
+	// processes := make(map[string]Process)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	var input string
+
 	go func() {
 		for scanner.Scan() {
 			input = scanner.Text()
-			output_channel := exe_cmd(input)
 
 			// currently this channel is not really used and the process
 			// prints directly to stdout.  we need to find a way to pipe
 			// this into the channel.
+
+			process := create_process(input)
+
 			go func() {
-				c <- <-output_channel
+				c <- <-process.channel
 			}()
 		}
 	}()
@@ -44,7 +78,7 @@ func read_input() <-chan string {
 	return c
 }
 
-func exe_cmd(cmd string) <-chan string {
+func exe_cmd(cmd, identifier string) chan string {
 	c := make(chan string)
 
 	parts := strings.Fields(cmd)
@@ -54,9 +88,14 @@ func exe_cmd(cmd string) <-chan string {
 	go func() {
 		if head == "cd" {
 			os.Chdir(parts[0])
-			c <- fmt.Sprint()
+
+			c <- fmt.Sprintln()
+		} else if head == "send" {
+			process_identifier := parts[0]
+			cmd := strings.Join(parts[1:], " ")
+			send_input_to(process_identifier, cmd)
 		} else {
-			// write now, output does not go into the channel;
+			// right now, output does not go into the channel;
 			// it prints directly to the stdout.  find a way to
 			// send it into the channel.
 			cmd := exec.Command(head, parts...)
@@ -64,13 +103,52 @@ func exe_cmd(cmd string) <-chan string {
 			cmd.Stderr = os.Stdout
 			cmd.Run()
 			c <- fmt.Sprintln()
+
+			go func() {
+				var input string
+				for {
+					input = <-c
+					run_cmd(input)
+				}
+			}()
 		}
+		// listen for additional commands to this process
+		// delete the process after it is complete
+		delete(PROCESSES, identifier)
 	}()
 
 	return c
 }
 
+// does not keep track of channels or attempt ot route special commands
+// like 'cd' or 'send'
+func run_cmd(command string) {
+	parts := strings.Fields(command)
+	head := parts[0]
+	parts = parts[1:len(parts)]
+	cmd := exec.Command(head, parts...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	cmd.Run()
+}
+
+func send_input_to(identifier, input string) {
+	if process, ok := PROCESSES[identifier]; ok {
+		channel := process.channel
+		channel <- fmt.Sprint(input)
+	} else {
+	}
+}
+
 func prompt() string {
+	var prompt []string
+	for k, v := range PROCESSES {
+		prompt = append(prompt, fmt.Sprintf("[%s - %s]", k, v.command))
+	}
+
 	wd, _ := os.Getwd()
-	return fmt.Sprintf("%s >>> ", wd)
+
+	prompt = append(prompt, fmt.Sprintf("\n%s >>> ", wd))
+
+	return strings.Join(prompt, " ")
 }
